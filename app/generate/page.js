eb5c67406f5a091f8671a2047b5ffdb420ec3a10
Prateek Yadav <prateekyadavwork@gmail.com>
2025-08-29 06:36:07 +0530

    Update page.js

--- app/generate/page.js
+++ app/generate/page.js
@@ -1,4 +1,5 @@
 "use client";
+export const dynamic = "force-dynamic"
 import React, { useState } from "react";
 import { ToastContainer, toast } from "react-toastify";
 import "react-toastify/dist/ReactToastify.css";
@@ -175,5 +176,6 @@ const Generate = () => {
 };
 
 export default Generate;
+
 
 
