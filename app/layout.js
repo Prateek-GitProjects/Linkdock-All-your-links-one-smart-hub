763c013a36e339c11fef823d29b8cc1023bc2e1c
Prateek Yadav <prateekyadavwork@gmail.com>
2025-08-29 13:58:55 +0530

    Update layout.js

--- app/layout.js
+++ app/layout.js
@@ -30,9 +30,9 @@ export default function RootLayout({ children }) {
         className={`${geistSans.variable} ${geistMono.variable} antialiased`}
       >
         <Navbar/>
-        <suspense>
+        <Suspense>
         {children}
-        </suspense>
+        </Suspense>
       </body>
     </html>
   );
