497c0cc18909c483babc696c6824416afa242745
Prateek Yadav <prateekyadavwork@gmail.com>
2025-08-29 16:14:50 +0530

    Update page.js

--- app/generate/page.js
+++ app/generate/page.js
@@ -40,7 +40,7 @@ const Generate = () => {
       redirect: "follow",
     };
 
-    const r = await fetch("http://localhost:3000/api/add", requestOptions);
+    const r = await fetch("/api/add", requestOptions);
     const result = await r.json();
     if (result.success) {
       toast.success(result.message);
@@ -171,6 +171,7 @@ const Generate = () => {
 };
 
 export default Generate;
+
 
 
 
